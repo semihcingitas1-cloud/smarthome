676c15e54aaf51f9f7dbf0fbedb2ba0c972da3d6
semihcingitas1-cloud <semihcingitas1@gmail.com>
2026-04-14 16:53:55 +0300

    Update devicesSlice.js

--- src/redux/devicesSlice.js
+++ src/redux/devicesSlice.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = "http://104.196.18.94:4000";
 
 const initialState = {
   devices: [],
