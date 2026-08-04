06cf5b16b3e6330490c6d1c16567bd213ab030e6
semih <semihcingitas1@gmail.com>
2026-08-05 00:21:42 +0300

    Update productSlice.js

--- src/redux/productSlice.js
+++ src/redux/productSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const BASE_URL = "https://backend-d72l.onrender.com";
+const BASE_URL = "152.70.44.174:4000";
 
 const getToken = () => localStorage.getItem("token");
 
